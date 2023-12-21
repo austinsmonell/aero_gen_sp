185f2ad03e1e5d59103cff9eb7158706397f75fa
austinmonell <austmonell67@gmail.com>
2023-12-21 02:34:54 -0700

    [LQR] completed LQR method on linearized inner and outer loop of cascaded feedback contoller

--- analyze_actuator_control.m
+++ analyze_actuator_control.m
@@ -2,16 +2,33 @@ clear
 clc
 close all
 
-%% Get State Space
-K = [1, 0, 0, 0, 0, 0];
-x0 = [0; 0; 0; 0; 0; 0];
+%% Get State Space Inner
+K_inner_act = [0, 0];
+x0 = [0; 0];
 u0 = [0];
-[A, B, C, D] = linmod("actuator_controller", x0, u0);
-[num, den] = ss2tf(A, B, C, D);
-gv = minreal(tf(num, den));
+[A_inner, B_inner, C, D] = linmod("actuator_controller_inner", x0, u0);
+[num, den] = ss2tf(A_inner, B_inner, C, D);
+gv_inner = minreal(tf(num, den));
 
-%% LQR
-Q = [1 0 0 0 0 0; 0 0 0 0 0 0; 0 0 1 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 0; 0 0 0 0 0 10];
-R = 5;
+%% LQR Inner
+Q_inner = [1 0; 0 1];
+R_inner = 0.0012;
 
-[K, S, E] = lqr(A, B, Q, R)
+[K_inner, ~, E_inner] = lqr(A_inner, B_inner, Q_inner, R_inner);
+K_inner_act = K_inner*0.05
+
+
+%% Get State Space Outer
+K_outer = [0, 0, 0, 0];
+x0 = [0; 0; 0; 0];
+u0 = [0];
+[A_outer, B_outer, C, D] = linmod("actuator_controller_outer", x0, u0);
+[num, den] = ss2tf(A_outer, B_outer, C, D);
+gv_outer = minreal(tf(num, den));
+
+%% LQR Outer
+Q_outer = [1 0 0 0; 0 0 0 0; 0 0 0 0; 0 0 0 0];
+R_outer = 50;
+
+[K_outer, ~, E_outer] = lqr(A_outer, B_outer, Q_outer, R_outer);
+K_outer_act = [K_outer(1), K_outer(2)*0.05, K_outer(3), K_outer(4)*0.05]
